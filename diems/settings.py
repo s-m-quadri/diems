22df7131aed4c79f965aaee7448247791fa6f61d
quadri <quadrisyedminnatullah@gmail.com>
2023-05-06 16:42:27 +0530

    v1.0.1 Enhanced Interface, made more accessible
    Here we have,
    1. Added simpler navigation,
    2. Modified whole interface,
    3. Not changed any functionality,
    4. Incresed accessibility
    etc...

--- diems/settings.py
+++ diems/settings.py
@@ -35,7 +35,7 @@ else:
         SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'diems.pythonanywhere.com']
 
